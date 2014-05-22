a114fce592fb78e9b065ffcb6424311ca8660e04
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2014-05-22 15:17:36 -0700

    Added missing quotes around variable name in output.

--- analysis/unused.d
+++ analysis/unused.d
@@ -300,7 +300,7 @@ class UnusedVariableCheck : BaseAnalyzer
 			if (!uu.isRef)
 				addErrorMessage(uu.line, uu.column,
 					(uu.isParameter ? "Parameter " : "Variable ")
-					~ uu.name ~ " is never used");
+					~ "'" ~ uu.name ~ "' is never used");
 		}
 		tree = tree[0 .. $ - 1];
 	}
