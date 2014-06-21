ed4c56612b6416f1c967ac2f137df0d50823404f
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2014-06-21 16:28:36 -0700

    Updated the function purity checker to work on functions.

--- analysis/missing_purity.d
+++ analysis/missing_purity.d
@@ -14,14 +14,19 @@ import analysis.ast_helpers;
 import analysis.manager;
 import analysis.scope_frame;
 import analysis.scope_analyzer;
+import dlang_helper;
 
 
 /**
- * FIXME
+ * Checks for functions that can be pure.
  */
+// FIXME: Make it work with globals
+// FIXME: Make it work with methods
 class MissingPurityCheck : ScopeAnalyzer
 {
 	alias visit = ScopeAnalyzer.visit;
+	private bool[string] calls;
+	private bool[string][string] dependencies;
 
 	this(string fileName)
 	{
@@ -59,39 +64,95 @@ class MissingPurityCheck : ScopeAnalyzer
 		}
 	}
 
+	override void visit(const FunctionCallExpression node)
+	{
+		string name = getFunctionCallName(scopeManager.scope_, node);
+		auto funcData = scopeManager.scope_.getFunction(name);
+
+		// If the function is unknown, assume that it is not pure
+		bool isPure = false;
+		if (funcData is FunctionData.init)
+			stderr.writefln("!!! failed to find function '%s'", name);
+		else
+			isPure = funcData.isPure;
+
+		calls[name] = isPure;
+		node.accept(this);
+	}
+
 	override void visit(const FunctionDeclaration node)
 	{
-		auto data = getFunctionData(scopeManager.scope_, node);
-		writefln("!!! function name:%s, isPure:%s", data.name, data.isPure);
+		// Get the name of this function
+		string name = node.name.text;
+		auto funcData = scopeManager.scope_.getFunction(name);
+		if (funcData is FunctionData.init)
+		{
+			stderr.writefln("!!! failed to find function '%s'", name);
+			node.accept(this);
+			return;
+		}
 
-		// FIXME: Here we need to walk over all function calls, and sub function
-		// calls to see they are pure too.
+		// Get the functions called by this function
+		calls.clear();
+		node.accept(this);
+
+		// Check all the called functions to see if they are pure
+		dependencies[name] = calls;
+		bool areDependenciesPure = areDependenciesPure(name);
+
+		// Warn if all the dependent functions are pure and this is not pure
+		if (areDependenciesPure && !funcData.isPure)
+		{
+			string message = "The function '%s' can be pure.".format(name);
+			size_t line = node.name.line;
+			size_t column = node.name.column;
+			addErrorMessage(line, column, message);
+		}
+	}
+
+	private bool areDependenciesPure(string name)
+	{
+		foreach (string callName, bool callPure; dependencies[name])
+		{
+			// Retrun false if this function is not pure
+			if (!callPure)
+				return false;
+
+			// Return false if this function calls any that are not pure
+			if (!areDependenciesPure(callName))
+				return false;
+		}
+
+		return true;
 	}
 }
 
 unittest
 {
 	assertAnalyzerWarnings(q{
-		pure int addPure(int a, int b)
+		void testFunctions()
 		{
-			return a + b;
-		}
+			pure int addPure(int a, int b)
+			{
+				return a + b;
+			}
 
-		int addNonPure(int a, int b)
-		{
-			return a + b;
-		}
+			int addNonPure(int a, int b) // [warn]: The function 'addNonPure' can be pure.
+			{
+				return a + b;
+			}
 
-		// Can be pure because addPure is pure
-		int aaa(int a, int b) // FIXME: [warn]: Function 'aaa' can be pure.
-		{
-			return addPure(a, b);
-		}
+			// Can be pure because addPure is pure
+			int aaa(int a, int b) // [warn]: The function 'aaa' can be pure.
+			{
+				return addPure(a, b);
+			}
 
-		// Can't be pure because addNonPure is not pure
-		int bbb(int a, int b)
-		{
-			return addNonPure(a, b);
+			// Can't be pure because addNonPure is not pure
+			int bbb(int a, int b)
+			{
+				return addNonPure(a, b);
+			}
 		}
 	}c, analysis.run.AnalyzerCheck.missing_purity);
 
